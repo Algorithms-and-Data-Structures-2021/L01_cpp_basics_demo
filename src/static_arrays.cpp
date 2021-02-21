0736bdd2d6f25d64104fc36dbbb9f01b094221f2
Ramil Safin <RamilSafNab1996@gmail.com>
2021-02-21 11:28:38 +0300

    Add multi-dimensional arrays explanation

--- src/static_arrays.cpp
+++ src/static_arrays.cpp
@@ -157,6 +157,9 @@ int main() {
     // можно думать о них, как об указателях на начало и конец массива
     fill(cpp_int_arr.begin(), cpp_int_arr.end(), /* значения элементов */0);
 
+    // или при помощи метода класса array
+    cpp_int_arr.fill(0);
+
     // устанавливаем последние два элемента массива в значение 1
     fill(cpp_int_arr.end() - 2, cpp_int_arr.end(), 1);
 
@@ -166,16 +169,19 @@ int main() {
         cout << element << '\t';
     }
 
-    cout << endl;
+    // массив в стиле Си можно тоже заполнить каким-либо значением при помощи функции fill
+    int int_arr6[3] = {};
+    fill(/* указатель на начало массива */ int_arr6,
+            /* указатель на элемент за концом массива */int_arr6 + 3,
+            /* значение */ 6);
 
     // копирование массивов может осуществляться по-разному
     // если мы работаем с массивом в стиле Си, то придется использовать функцию копирования copy
     int copy_int_arr[5];
 
-    copy(/* указатель на первый элемент */ int_arr,
-            /* указатель на элемент после последнего элемента */int_arr + 5,
-            // если установить int_arr + 4, то последний элемент не скопируется
-            /* куда положить результат, дургой массив */ copy_int_arr);
+    copy(/* указатель на начало массива */ int_arr,
+            /* указатель на элемент за концом массива */int_arr + 5,
+            /* куда положить результат, другой массив */ copy_int_arr);
 
     cout << "\nFor-each loop copy_int_arr (copy):" << endl;
 
@@ -193,7 +199,7 @@ int main() {
 
     cout << endl;
 
-    // с C++ массивами все гораздо проще
+    // копирование C++ массивов
     array<int, 5> copy_cpp_int_arr = cpp_int_arr;  // массив автоматически скопируется (склонируется)
     // здесь используется поведение оператора копирования = (равно)
 
@@ -213,8 +219,84 @@ int main() {
 
     cout << endl;
 
+    // многомерные статические массивы (матрицы)
+    int array_2d[/* кол-во строк */3][/* кол-во столбцов */2] = {{1, 3},
+                                                                 {5, 7},
+                                                                 {9, 11}};
+
+    // можно инициализировать массив следующим эквивалентным образом
+    // int array_2d[3][2] = {1, 3, 5, 7, 9, 11}
+
+    // указатели на начало строк массива (на первые элементы строк)
+    int *array_2d_0th_row = array_2d[0];
+    int *array_2d_1st_row = array_2d[1];
+    int *array_2d_2nd_row = array_2d[2];
+
+    cout << "\nAddresses and values of 0th elements in rows:" << endl;
+
+    printf("array_2d[0] at %d: %d\n", array_2d_0th_row, *array_2d_0th_row);
+    printf("array_2d[1] at %d: %d\n", array_2d_1st_row, *array_2d_1st_row);
+    printf("array_2d[2] at %d: %d\n\n", array_2d_2nd_row, *array_2d_2nd_row);
+
+    // несмотря на то, что данные теперь разделены условными номерами строк,
+    // они все еще находятся последовательно в памяти
+    // [1, 3, 5, 7, 9, 11]
+
+    // проверим это при помощи указателей
+    int *array_2d_begin = &array_2d[0][0];  // указатель на начало массива (0 строка, 0 столбец)
+    int *array_2d_end = &array_2d[2][1];  // указатель на конец массива (2 строка, 1 столбец)
+
+    // цикл, в котором мы передвигаем указатель от начала массива к его концу
+    // pointer++ означает что мы передвигаемся от условного адреса ADDR к ADDR + sizeof(int)
+    for (int *pointer = array_2d_begin; pointer <= array_2d_end; pointer++) {
+        printf("array_2d at %d: %d\n", pointer, *pointer);
+    }
+
+    cout << endl;
+
+    // доступ к элементам массива можно получить оператором [] (квадратные скобки)
+    // пройдемся по элементам двумерного массива в цикле
+    for (int row_index = 0; row_index < 3; row_index++) {
+
+        cout << "Iterating over a row #" << row_index << endl;
+
+        for (int column_index = 0; column_index < 2; column_index++) {
+
+            // получение элемента массива на определенной строке и столбце
+            int element = array_2d[row_index][column_index];
+
+            printf("\tarray_2d[%d][%d] = %d\n", row_index, column_index, element);
+        }
+
+        cout << endl;
+    }
+
+    // многомерные массивы в стиле C++
+
+    // массив array<..., 3> из массивов array<int, 2>
+    // 3 строки и 2 столбца
+    array<array<int, 2>, 3> cpp_array_2d = {};  // инициализируем нулями
+
+    // получение кол-ва строк массива
+    int cpp_array_2d_num_rows = cpp_array_2d.size();
+    int cpp_array_2d_num_cols = cpp_array_2d[0].size();
+
+    printf("\ncpp_array_2d dimensions: %d x %d\n", cpp_array_2d_num_rows, cpp_array_2d_num_cols);
+
+    // доступ к элементам через оператор []
+    cpp_array_2d[0][0] = 1;
+
+    int cpp_array_2d_element = cpp_array_2d[0][1];
+
+    // for-each итерация массива
+    // можно заменить тип на кючевое слово auto
+    for (/* array<int, 2> */ auto cpp_array_row: cpp_array_2d) {
+        for (int element : cpp_array_row) {
+            // блок кода
+        }
+    }
+
     // передача массива в функцию
-    // многомерные статические массивы
 
     return 0;
 }
